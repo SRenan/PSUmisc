e87a609837e0ef611cb08377c51c6c596ea5e92d
SRenan <renan.sauteraud@gmail.com>
2018-07-30 11:02:41 -0400

    New function to find topSNP at each locus
    New function to find nearest position in a second vector
    New function to map ensembl transcript to gene symbol

--- R/GWASfuncs.R
+++ R/GWASfuncs.R
@@ -57,7 +57,7 @@ ggqqp <- function(pval, alpha = 1e-2){
   # The pvalues are assumed to follow a chisq with 1df
   lout <- length(pval)
   obs <- sort(pval)
-  sigobs <- obs[obs < alpha] 
+  sigobs <- obs[obs < alpha]
   nsig <- length(sigobs)
   nunsig <- lout - nsig
 
@@ -79,5 +79,65 @@ ggqqp <- function(pval, alpha = 1e-2){
 }
 
 
+#' Isolate most significant SNP at each locus
+#' @export
+topSNP <- function(pos, pval, ret = "idx", locus_len = 1e6){
+  # Order positions
+  if(any(sort(pos) != pos)){
+    stop("The positions are not ordered")
+  }
+  if(length(pos) == 1){
+    return(1)
+  }
+  # Split the data into loci
+  loci_start <- c(1, which(diff(pos) > locus_len)+1)
+  loci_end <- c(loci_start - 1, length(pos))[-1]
+  # Find max loci between start pos and end
+  idx <- sapply(seq_along(loci_start), function(XX){which.min(pval[loci_start[XX]:loci_end[XX]])})
+  idx <- idx + loci_start - 1
+  if(ret == "idx"){
+    ret <- idx
+  } else if(ret == "pos"){
+    ret <- pos[idx]
+  }
+  return(ret)
+}
+
+#' Subset a table for the top SNP in each locus
+#'
+#' @param table A \code{data.table} of significantly associated SNPs, with the
+#'  columns listed in the next 3 arguments. Additional columns are preserved
+#' @param pos A \code{character}. The name of the column of positions in \code{table}.
+#' @param pval A \code{character}. The name of the column of p-values in \code{table}.
+#' @param group A \code{character}. The name of the column used for grouping.
+#'  Typically phenotype. Locus are considered independently for each group.
+#' @param locus_len A \code{numeric}. The size of loci. i.e: One SNP get returned
+#'  for each window of locus_len.
+#'
+#' @return A subset of the original \code{table} containing only the most
+#' significant SNP at each locus.
+#'
+#' @export
+topTableSNP <- function(table, pos, pval, group, locus_len = 1e6){
+  tab2 <- table[, topSNP(get(pos), get(pval), locus_len = locus_len),  by = group]
+  ret <- table[tab2, .SD[i.V1], on = group, by = .EACHI]
+  return(ret)
+}
 
 
+
+#' Find position from RSID
+#'
+#' @import SNPlocs.Hsapiens.dbSNP144.GRCh37
+#' @import TxDb.Hsapiens.UCSC.hg19.knownGene
+#' @importFrom BSgenome snpsById
+#' @importFrom GenomicFeatures genes
+#' @importFrom GenomeInfoDb keepStandardChromosomes seqlevelsStyle
+annotateSNP <- function(rsids){
+  snps <- SNPlocs.Hsapiens.dbSNP144.GRCh37
+  gp <- snpsById(snps, rsids, ifnotfound = "drop")
+  seqlevelsStyle(gp) <- "UCSC" # "chrX"
+
+  dt <- data.table(gp)
+  return(dt)
+}
---
+++ R/annotations.R
@@ -0,0 +1,30 @@
+#' Find closest position in a reference
+#'
+#' @param pos A \code{numeric}. The list of position to match.
+#' @param ref The \code{numeric} vector of postions to look into.
+#' @export
+find_nearest_pos <- function(pos, ref){
+  idxs <- sapply(pos, function(XX) which.min(abs(XX - ref)))
+  return(idxs)
+}
+
+#' Transcript to gene symbol
+#'
+#' Use biomaRt to obtain an annotation data.table that maps from ensembl
+#' transcript to gene symbol.
+#'
+#' @param enst A \code{character} vector. Unless NULL, the annotation will be
+#' subset for the trancripts specified.
+#'
+#' @importFrom biomaRt useMart
+#' @export
+enst2symbol <- function(enst = NULL){
+  mart <- useMart(biomart="ensembl", dataset="hsapiens_gene_ensembl")
+  e2g <- data.table(getBM(attributes = c("hgnc_symbol", "ensembl_transcript_id"),
+                           filters = "ensembl_transcript_id", values = trs, mart = mart))
+  setnames(e2g, c("GENE", "transcript"))
+  if(is.null(enst)){
+    e2g <- e2g[transcript %in% est]
+  }
+  return(e2g)
+}
